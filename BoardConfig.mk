e6e2c4a4eafb63b2626e4d52a7270de96299907b
Louis Popi <theh2o64@gmail.com>
2016-05-29 11:36:55 +0200

    msm8916_64: Use appened DTB

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,10 +8,11 @@ TARGET_BOOTLOADER_BOARD_NAME := msm8916
 
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 ifeq ($(PRODUCT_BRAND),YU)
-TARGET_KERNEL_APPEND_DTB := false
+BOARD_KERNEL_SEPARATED_DT := false
 else
-TARGET_KERNEL_APPEND_DTB := true
+BOARD_KERNEL_SEPARATED_DT := true
 endif
+TARGET_KERNEL_APPEND_DTB := true
 
 BOARD_USES_GENERIC_AUDIO := true
 USE_CAMERA_STUB := true
@@ -71,7 +72,6 @@ TARGET_USES_CSVT := true
 ifneq ($(PRODUCT_BRAND),YU)
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 endif
-BOARD_KERNEL_SEPARATED_DT := true
 
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 2048
